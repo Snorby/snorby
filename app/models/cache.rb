db7b5e391679b6a4b2ac92262d8e6138e3321fe0
Dustin Willis Webber <dustin.webber@gmail.com>
2010-10-31 18:54:39 -0400

    Added cache table

---
+++ app/models/cache.rb
@@ -0,0 +1,35 @@
+class Cache
+
+  include DataMapper::Resource
+
+  property :id, Serial
+
+  property :sid, Integer
+  
+  property :cid, Integer
+
+  property :ran_at, DateTime
+  
+  property :event_count, Integer
+  
+  property :tcp_count, Integer
+  
+  property :udp_count, Integer
+  
+  property :icmp_count, Integer
+  
+  property :total_src, Integer
+  
+  property :total_dst, Integer
+  
+  property :uniq_src, Integer
+  
+  property :uniq_dst, Integer
+  
+  property :port_metrics, Hash
+  
+  property :classification_metrics, Hash
+  
+  property :severity_metrics, Hash
+
+end
