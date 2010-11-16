91f1915a95331506909fd36b1294af86769af6fa
Dustin Willis Webber <dustin.webber@gmail.com>
2010-11-16 13:08:01 -0500

    Added signature table to event view

--- app/models/signature.rb
+++ app/models/signature.rb
@@ -38,5 +38,21 @@ class Signature
   def name
     sig_name
   end
+  
+  #
+  #  
+  # 
+  def event_percentage(in_wrods=false)
+    begin
+      total_event_count = Signature.all.map(&:events_count).sum
+      if in_wrods
+        "#{self.events_count}/#{total_event_count}"
+      else
+        ((self.events_count.to_f / total_event_count.to_f) * 100).round
+      end
+    rescue FloatDomainError
+      0
+    end
+  end
 
 end
