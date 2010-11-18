b1490b4dc389da7274a0683a2a0fe3bff939aca8
Dustin Willis Webber <dustin.webber@gmail.com>
2010-11-18 00:01:23 -0500

    move dashboard work - added event chart vs time by sensor

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -8,16 +8,27 @@ class PageController < ApplicationController
     @udp ||= @cache.map(&:udp_count)
     @icmp ||= @cache.map(&:icmp_count)
     
+    @high ||= @cache.severity_count(:high)
+    @medium ||= @cache.severity_count(:medium)
+    @low ||= @cache.severity_count(:low)
+    
+    @event_count ||= @cache.all.map(&:event_count).sum
+    
+    @sensor_metrics ||= Cache.sensor_metrics
     @classification_metrics ||= @cache.classification_metrics.sort
     
     @tcp_total ||= DailyCache.all.map(&:tcp_count).sum
     @udp_total ||= DailyCache.all.map(&:udp_count).sum
     @icmp_total ||= DailyCache.all.map(&:icmp_count).sum
-    @event_count ||= DailyCache.all.map(&:event_count).sum
     
     @classifications ||= Classification.all(:order => [:events_count.desc])
     @sensors ||= Sensor.all(:limit => 5, :order => [:events_count.desc])
     @favers ||= User.all(:limit => 5, :order => [:favorites_count.desc])
+    
+    @last_cache = @cache.last.ran_at
+    
+    sigs = Event.all(:limit => 5, :order => [:timestamp.desc], :fields => [:sig_id], :unique => true).map(&:signature).map(&:sig_id)
+    @recent_events ||= Event.all(:sig_id => sigs).group_by { |x| x.sig_id }.map(&:last).map(&:first)
     
   end
   
--- app/models/cache.rb
+++ app/models/cache.rb
@@ -5,36 +5,36 @@ class Cache
   property :id, Serial
 
   property :sid, Integer
-  
+
   property :cid, Integer
 
   property :ran_at, DateTime
-  
+
   property :event_count, Integer, :default => 0
-  
+
   property :tcp_count, Integer, :default => 0
-  
+
   property :udp_count, Integer, :default => 0
-  
+
   property :icmp_count, Integer, :default => 0
-  
+
   property :classification_metrics, Object
-  
+
   property :severity_metrics, Object
-  
+
   property :signature_metrics, Object
-  
+
   # Define created_at and updated_at timestamps
   timestamps :at
-  
+
   belongs_to :sensor, :parent_key => :sid, :child_key => :sid
-  
+
   has 1, :event, :parent_key => [ :sid, :cid ], :child_key => [ :sid, :cid ]
 
   def self.last_month
     all(:ran_at.gte => 2.month.ago.beginning_of_month, :ran_at.lte => 1.month.ago.end_of_month)
   end
-  
+
   def self.this_month
     all(:ran_at.gte => Time.now.beginning_of_month, :ran_at.lte => Time.now.end_of_month)
   end
@@ -42,7 +42,7 @@ class Cache
   def self.last_week
     all(:ran_at.gte => 2.week.ago.beginning_of_week, :ran_at.lte => 1.week.ago.end_of_week)
   end
-  
+
   def self.this_week
     all(:ran_at.gte => Time.now.beginning_of_week, :ran_at.lte => Time.now.end_of_week)
   end
@@ -55,10 +55,41 @@ class Cache
     all(:ran_at.gte => Time.now.beginning_of_day, :ran_at.lte => Time.now.end_of_day)
   end
 
+  def self.severity_count(type)
+    @cache = self.map(&:severity_metrics)
+    count = []
+    case type.to_sym
+    when :high
+      @cache.each { |x| count << (x.has_key?(1) ? x[1] : 0) }
+    when :medium
+      @cache.each { |x| count << (x.has_key?(2) ? x[2] : 0) }
+    when :low
+      @cache.each { |x| count << (x.has_key?(3) ? x[3] : 0) }
+    end
+    count
+  end
+
+  def self.sensor_metrics
+    @metrics = []
+
+    Sensor.all(:limit => 5, :order => [:events_count.desc]).each do |sensor|
+      count = Array.new(24) { 0 }
+      blah = sensor.cache.today.group_by { |x| x.ran_at.hour }
+
+      blah.each do |hour, data|
+        count[hour] = data.map(&:event_count).sum
+      end
+
+      @metrics << { :name => sensor.name, :data => count }
+    end
+    
+    @metrics
+  end
+
   def self.classification_metrics
     @cache = self.map(&:classification_metrics)
     @classifications = []
-    
+
     Classification.each do |classification|
       count = 0
       @cache.each do |cache|
