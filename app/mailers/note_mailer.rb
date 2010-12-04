726be93fcde3824d128d0797855beb925267a860
Dustin Willis Webber <dustin.webber@gmail.com>
2010-12-04 00:57:02 -0500

    type fix and note mailer fixup

--- app/mailers/note_mailer.rb
+++ app/mailers/note_mailer.rb
@@ -3,15 +3,9 @@ class NoteMailer < ActionMailer::Base
   def new_note(note)
     @note = note
     @event = @note.event
-    @users = []
-    
-    User.all.each do |user|
-      @users << "#{user.name}, <#{user.email}>" if user.accepts_note_notifications?(@event)
-    end
+    @emails = User.all.collect { |user| "#{user.name} <#{user.email}>" if user.accepts_note_notifications?(@event) }.join(',')
 
-    mail(:to => @users.join(','), 
-    :from => Setting.find(:email) || "snorby@snorby.org", 
-    :subject => "[Snorby] New Event Note Added")
+    mail(:to => @emails, :from => (Setting.email? ? Setting.find(:email) : "snorby@snorby.org"), :subject => "[Snorby] New Event Note Added")
   end
   
 end
