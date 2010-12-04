474a709d209b91801b37215e0ae21b1a48e36b2b
Dustin Willis Webber <dustin.webber@gmail.com>
2010-12-04 01:36:06 -0500

    working on email error

--- app/mailers/note_mailer.rb
+++ app/mailers/note_mailer.rb
@@ -1,11 +1,12 @@
 class NoteMailer < ActionMailer::Base
-  
+
   def new_note(note)
     @note = note
     @event = @note.event
     @emails = User.all.collect { |user| user.accepts_note_notifications?(@event) ? "#{user.name} <#{user.email}>" : "" }.join(',')
 
+    return if @emails.blank?
     mail(:to => @emails, :from => (Setting.email? ? Setting.find(:email) : "snorby@snorby.org"), :subject => "[Snorby] New Event Note Added")
   end
-  
+
 end
