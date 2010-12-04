437fc72e54654f73a640271d5295318984846eaf
Dustin Willis Webber <dustin.webber@gmail.com>
2010-12-04 01:04:39 -0500

    fixed nil emails for note mailer

--- app/mailers/note_mailer.rb
+++ app/mailers/note_mailer.rb
@@ -3,7 +3,7 @@ class NoteMailer < ActionMailer::Base
   def new_note(note)
     @note = note
     @event = @note.event
-    @emails = User.all.collect { |user| "#{user.name} <#{user.email}>" if user.accepts_note_notifications?(@event) }.join(',')
+    @emails = User.all.collect { |user| user.accepts_note_notifications?(@event) ? "#{user.name} <#{user.email}>" : "" }.join(',')
 
     mail(:to => @emails, :from => (Setting.email? ? Setting.find(:email) : "snorby@snorby.org"), :subject => "[Snorby] New Event Note Added")
   end
