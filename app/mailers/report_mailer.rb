eb3299cd3a32bb0edcbbf3ab5b121c25b2fe18ac
Dustin Willis Webber <dustin.webber@gmail.com>
2010-12-05 20:46:29 -0500

    working on report emails templates

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -5,7 +5,7 @@ class ReportMailer < ActionMailer::Base
     User.all.each { |user| @emails << "#{user.name} <#{user.email}>" }
     report = Snorby::Report.build_report('yesterday')
     attachments["snorby-daily-report.pdf"] = report[:pdf]
-    mail(:to => @emails, :from => (Setting.email? ? Setting.find(:email) : "snorby@snorby.org"), :subject => "Snorby Daily Report: #{report[:start_time]} - #{report[:end_time]}")
+    mail(:to => @emails, :from => (Setting.email? ? Setting.find(:email) : "snorby@snorby.org"), :subject => "Snorby Daily Report: #{report[:start_time].strftime('%A, %B %d, %Y %I:%M %p')} - #{report[:end_time].strftime('%A, %B %d, %Y %I:%M %p')}")
   end
 
   def weekly_report
@@ -13,7 +13,7 @@ class ReportMailer < ActionMailer::Base
     User.all.each { |user| @emails << "#{user.name} <#{user.email}>" }
     report = Snorby::Report.build_report('week')
     attachments["snorby-weekly-report.pdf"] = report[:pdf]
-    mail(:to => @emails, :from => (Setting.email? ? Setting.find(:email) : "snorby@snorby.org"), :subject => "Snorby Weeklt Report: #{report[:start_time]} - #{report[:end_time]}")
+    mail(:to => @emails, :from => (Setting.email? ? Setting.find(:email) : "snorby@snorby.org"), :subject => "Snorby Weeklt Report: #{report[:start_time].strftime('%A, %B %d, %Y %I:%M %p')} - #{report[:end_time].strftime('%A, %B %d, %Y %I:%M %p')}")
   end
   
   def monthly_report
@@ -21,7 +21,7 @@ class ReportMailer < ActionMailer::Base
     User.all.each { |user| @emails << "#{user.name} <#{user.email}>" }
     report = Snorby::Report.build_report('month')
     attachments["snorby-monthly-report.pdf"] = report[:pdf]
-    mail(:to => @emails, :from => (Setting.email? ? Setting.find(:email) : "snorby@snorby.org"), :subject => "Snorby Monthly Report: #{report[:start_time]} - #{report[:end_time]}")
+    mail(:to => @emails, :from => (Setting.email? ? Setting.find(:email) : "snorby@snorby.org"), :subject => "Snorby Monthly Report: #{report[:start_time].strftime('%A, %B %d, %Y %I:%M %p')} - #{report[:end_time].strftime('%A, %B %d, %Y %I:%M %p')}")
   end
 
 end
