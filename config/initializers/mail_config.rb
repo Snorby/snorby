d1dbb0931fb7eb7052ca7e2a67549325bbe89f20
Dustin Willis Webber <dustin.webber@gmail.com>
2011-01-16 17:40:59 -0500

    fixed some format issues with the mail_config

--- config/initializers/mail_config.rb
+++ config/initializers/mail_config.rb
@@ -1,6 +1,8 @@
 # Snorby Mail Configuration
 
+# #
 # Gmail Example:
+# 
 # ActionMailer::Base.delivery_method = :smtp
 # ActionMailer::Base.smtp_settings = {
 #   :address              => "smtp.gmail.com",
@@ -12,12 +14,15 @@
 #   :enable_starttls_auto => true
 # }
 
+# #
 # Sendmail Example:
+# 
 # ActionMailer::Base.delivery_method = :sendmail
 # ActionMailer::Base.sendmail_settings = {
 #   :location => '/usr/sbin/sendmail',
 #   :arguments => '-i -t'
 # }
+
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
 
