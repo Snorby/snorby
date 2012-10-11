d7bcaa9af888f0ef8dc3a601dd547b8f1019f4d0
Dustin Willis Webber <dustin.webber@gmail.com>
2012-10-11 17:17:27 -0400

    more route updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,12 +12,28 @@ class SessionsController < Devise::SessionsController
     scope = Devise::Mapping.find_scope!(resource_or_scope)
     resource ||= resource_or_scope
     sign_in(scope, resource) unless warden.user(scope) == resource
-    return render :json => {:success => true, :users => @current_user, :redirect => stored_location_for(scope) || after_sign_in_path_for(resource)}
+    return render :json => {:success => true, :user => @current_user, :redirect => stored_location_for(scope) || after_sign_in_path_for(resource)}
   end
 
   def failure
     return render:json => {:success => false, :errors => ["Login failed."]}
   end
+
+  # DELETE /resource/sign_out
+  def destroy
+    redirect_path = after_sign_out_path_for(resource_name)
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message :notice, :signed_out if signed_out && is_navigational_format?
+
+    # We actually need to hardcode this as Rails default responder doesn't
+    # support returning empty response on GET request
+    respond_to do |format|
+      format.html(*navigational_formats) { redirect_to redirect_path }
+      format.html do
+        head :no_content
+      end
+      format.json { render :json => { status: "success", user: @current_user }}
+    end
 
 end
 
