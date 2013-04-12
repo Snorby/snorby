eefee007902bba6709f19370eeb01440f664e245
wayfairmike <mtanderson@wayfair.com>
2013-04-12 10:59:19 -0300

    Now with https flag for action_mailer
    Used to send the correct url in email if https is enabled

--- config/application.rb
+++ config/application.rb
@@ -88,7 +88,11 @@ module Snorby
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
-    config.action_mailer.default_url_options = { :host => Snorby::CONFIG[:domain] }
+    if CONFIG.has_key(:https) && CONFIG[:https]
+      config.action_mailer.default_url_options = { :protocol => 'https', :host => Snorby::CONFIG[:domain] }
+    else
+      config.action_mailer.default_url_options = { :host => Snorby::CONFIG[:domain] }
+    end
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
